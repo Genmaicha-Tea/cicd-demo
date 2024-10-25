c147cf1419d5c14c8b719000904f2e50deb6954c
Beomjun Kim <d1365227@ncsdevnet.com>
2024-10-25 11:48:30 +0800

    first commit

---
+++ src/main/java/sg/edu/nus/iss/cicd_demo2/CicdDemo2Application.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicd_demo2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdDemo2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CicdDemo2Application.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/cicd_demo2/CicdDemo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.cicd_demo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
